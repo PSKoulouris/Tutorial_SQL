2cfb672e3ffeb3fc57c05d3c102db55c91550390
PSKoulouris <philippe.koulouris@gmail.com>
2025-07-24 16:52:15 +0200

    view  one to many

--- index_student_OneToMany.php
+++ index_student_OneToMany.php
@@ -28,6 +28,27 @@
 */
 
 
+$sql = "SELECT *
+        FROM students AS A
+        INNER JOIN assignment AS B
+        WHERE  A.id = B.student_id";
+$results = $conn->query($sql);
+
+if ($results->num_rows>0) {
+    while($row = $results->fetch_assoc()){
+
+        if ($row["is_done"] == 0) {
+            $submissionStatus = "Submitted";
+        } else {
+            $submissionStatus = "Due";
+        }
+        echo "Id: " . $row["id"] . " Email: " . $row["email"] . " Assignment type: " . $row["name"] . " Assignment submission: " . $submissionStatus . " Submission date: " . $row["target_date"] . "<br>";
+    }
+} else {
+    echo "No data available";
+}
+
+
 
 
 
