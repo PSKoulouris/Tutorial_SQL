eb6c68b3b37c3befefe29ddb431398eeec388529
PSKoulouris <philippe.koulouris@gmail.com>
2025-08-31 18:47:24 +0200

    insert many to many

--- insert_ManyToMany.php
+++ insert_ManyToMany.php
@@ -6,9 +6,14 @@ error_reporting(E_ALL);
 
 include "database_connection.php";
 
+//Field submissions:
 
-
-
+$course_category = $_POST["name_course_category"];
+$course_credits = $_POST["name_course_credits"];
+$course_name = $_POST["name_course_name"];
+$assignment_is_done = $_POST["name_assignment_is_done"];
+$assignment_name = $_POST["name_assignment_name"];
+$assignment_date = $_POST["name_assignemnt_date"];
 
 
 
